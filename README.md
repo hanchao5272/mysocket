44d0e83f5c8cade213e043d2e4375b7441cacfdb
hanchao5272 <89298141@qq.com>
2018-05-03 23:00:51 +0800

    demo01-原始echo
    最简单的echo服务器：将客户端发送的内容再原封不动的反馈给客户端，只进行一次。
    服务端
    01.建立服务端监听：对端口号9527的socket请求进行监听
       serverListener = new ServerSocket(9527);
    02.阻塞的去获取一次连接
       onceSocket = serverListener.accept();
    03.获取输入流，接收数据=》处理数据=》获取输出流，返回数据=》关闭输出输入流
    04.关闭本次连接onceSocket.close();
    05.关闭服务监听serverListener.close();
    客户端
    01.建立客户端网络连接：建立一个到主机"127.0.0.1"的9527端口号的网络连接
       clientSocket = new Socket("127.0.0.1", 9527);
    02.获取输出流，发送数据=》获取输入流，接收数据=》关闭输入输出流
    03.关闭客户端连接clientSocket.close();

--- README.md
+++ README.md
@@ -12,3 +12,17 @@ Java网络编程的简单学习
 - InetAddress.getLocalHost():获取本机网络地址
 
 ## demo01-原始echo
+最简单的echo服务器：将客户端发送的内容再原封不动的反馈给客户端，只进行一次。
+服务端
+01.建立服务端监听：对端口号9527的socket请求进行监听
+   serverListener = new ServerSocket(9527);
+02.阻塞的去获取一次连接
+   onceSocket = serverListener.accept();
+03.获取输入流，接收数据=》处理数据=》获取输出流，返回数据=》关闭输出输入流
+04.关闭本次连接onceSocket.close();
+05.关闭服务监听serverListener.close();
+客户端
+01.建立客户端网络连接：建立一个到主机"127.0.0.1"的9527端口号的网络连接
+   clientSocket = new Socket("127.0.0.1", 9527);
+02.获取输出流，发送数据=》获取输入流，接收数据=》关闭输入输出流
+03.关闭客户端连接clientSocket.close();
---
+++ src/main/java/pers/hanchao/mysocket/demo01/client/SimpleSocketClient01.java
@@ -0,0 +1,55 @@
+package pers.hanchao.mysocket.demo01.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * <p>最简单的Socket客户端-进行一次会话</p>
+ *
+ * @author hanchao 2018/5/3 22:35
+ **/
+public class SimpleSocketClient01 {
+    /**
+     * <p></p>
+     *
+     * @author hanchao 2018/5/3 22:35
+     **/
+    public static void main(String[] args) {
+        //定义客户端连接
+        Socket clientSocket = null;
+        //定义输出流
+        OutputStream os = null;
+        //定义输入流
+        InputStream is = null;
+        //注意抛出异常：IOException
+        try {
+            //建立客户端网络连接：建立一个到主机"127.0.0.1"的9527端口号的网络连接
+            clientSocket = new Socket("127.0.0.1", 9527);
+            //获取输出流通道
+            os = clientSocket.getOutputStream();
+            //向服务端发送hello world
+            os.write("Hello World!".getBytes());
+            //获取输入流通道
+            is = clientSocket.getInputStream();
+            //获取服务的返回的数据
+            byte[] bytes = new byte[1024];
+            int length = is.read(bytes);
+            //输出从服务的获取的数据
+            System.out.println("服务端的反馈信息：" + new String(bytes, 0, length));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //关闭输入输出流
+                is.close();
+                os.close();
+                //关闭客户端连接
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
